735c2e078274950dd347dd308666892cbf0fc2d0
23052022 <golovataya.olya@gmail.com>
2022-09-01 11:46:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,139 @@
+from flask import Flask
+from flask import request
+import sqlite3
+import datetime
+from models import Account, Rating, Deposit, Currency, TransactionHistory, User
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+db.init_app(app)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+        return d
+
+def get_data(querry: str) -> object:
+    conn = sqlite3.connect('db1.db')
+    conn.row_factory = dict_factory
+    cursor = conn.execute(querry)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def add_data(querry: str):
+    conn = sqlite3.connect('db1.db')
+    cursor = conn.cursor()
+    cursor.execute(querry)
+    conn.commit()
+    conn.close()
+
+
+
+@app.get('/currency/<currency_UPS>')
+def currency_list(currency_UPS):
+    res = Currency.query.filter_by(name=currency_UPS).all()
+    return [it.to_dict() for it in res]
+
+@app.get('/currency/<currency_UPS>/rating')
+def currency_rating(currency_UPS):
+    res = Rating.query.filter_by(currency_name=currency_UPS).all()
+    return [it.to_dict() for it in res]
+
+@app.get('/currency')
+def all_currency_rating():
+    res = Currency.query.all()
+    return [it.to_dict() for it in res]
+
+
+@app.get('/currency/trade/main:<currency_UPS1>/second:<currency_UPS2>')
+def course_ups1_to_ups2(currency_UPS1, currency_UPS2):
+    date_now = datetime.datetime.now().strftime("%d-%m-%Y")
+    res = Currency.query.filter_by(name=currency_UPS1, date=date_now).first()
+    res1 = Currency.query.filter_by(name=currency_UPS2, date=date_now).first()
+    return {'exchange': f"res.USD_relative_value / res1.USD_relative_value"}
+
+
+
+
+@app.get('/user/<user_id>')
+def login_get(user_id):
+    res = User.query.filter_by(login=user_id).first()
+    return res
+
+
+@app.post('/currency/trade/<currency_UPS1>/<currency_UPS2>')
+def exchange(currency_UPS1, currency_UPS2):
+
+
+    req = request.json
+    date_now = datetime.datetime.now().strftime("%d-%m-%Y")
+    amount = req['data']['amount']
+    user_id = 1
+    user_balance1 = Account.query.filter_by(user_id=user_id, currency_name=currency_UPS1).first()
+    user_balance2 = Account.query.filter_by(user_id=user_id, currency_name=currency_UPS2).first()
+    cur1_USD_relative_value = Currency.query.filter_by(name=currency_UPS1, date=date_now).first()
+    cur2_USD_relative_value = Currency.query.filter_by(name=currency_UPS2, date=date_now).first()
+
+    need_cur2 = amount * cur1_USD_relative_value.USD_relative_value / cur2_USD_relative_value.USD_relative_value
+
+
+    if user_balance2.balance > need_cur2 and cur1_USD_relative_value.available_quantity > amount:
+        user_balance2.balance = user_balance2.balance - need_cur2
+        cur1_USD_relative_value.available_quantity = cur1_USD_relative_value.available_quantity + need_cur2
+        cur2_USD_relative_value.available_quantity = cur2_USD_relative_value.available_quantity - amount
+        user_balance1.balance = user_balance1.balance + amount
+
+        db.session.add(user_balance2)
+        db.session.add(cur1_USD_relative_value)
+        db.session.add(cur2_USD_relative_value)
+        db.session.add(user_balance1)
+        db.session.commit()
+
+        return "ok"
+    else:
+        return 'Error'
+
+
+@app.post('/currency/<name>/review')
+def currency_review_post(name):
+    req = request.json
+    cur_name = req['data']['currency_name']
+    rating = req['data']['rating']
+    comment = req['data']['comment']
+
+    review = Rating(currency_name=name, rating=rating, comment=comment)
+    db.session.add(review)
+    db.session.commit()
+    return 'OK'
+
+
+@app.put('/currency/<name>/review')
+def currency_review_put(name):
+    return f'Review currency {name}, PUT method'
+
+
+@app.delete('/currency/<name>/review')
+def currency_review_gelete(name):
+    return f'Review currency {name}, DELETE method'
+
+
+
+@app.post('/user/transfer')
+def transfer():
+   pass
+
+
+@app.get('/user/<user>/history')
+def user_history(user):
+
+    res = TransactionHistory.query.filter_by(user_id=user).all()
+    return [it.to_dict() for it in res]
+
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
---
+++ models.py
@@ -0,0 +1,98 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Account(db.Model):
+    id = db.Column(db.String(10), primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+    currency_name = db.Column(db.String(20), nullable=False)
+
+    def to_dict(self):
+        return {
+            "self.user_id": self.user_id,
+            'self.balance': self.balance,
+            'self.currency_name': self.currency_name
+        }
+
+class Currency(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    USD_relative_value = db.Column(db.Numeric, nullable=False)
+    available_quantity = db.Column(db.Numeric, nullable=False)
+    date = db.Column(db.String(10), nullable=False)
+
+    def to_dict(self):
+        return {
+            "self.name": self.name,
+            'self.USD_relative_value': self.USD_relative_value,
+            'self.available_quantity': self.available_quantity,
+            'self.date': self.date
+        }
+
+class Deposit(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer(), nullable=False)
+    opening_date = db.Column(db.Text, nullable=False)
+    closing_date = db.Column(db.Text)
+    balance = db.Column(db.Integer, nullable=False)
+    interest_rate = db.Column(db.Numeric, nullable=False)
+    conditions = db.Column(db.Text, nullable=False)
+
+
+    def to_dict(self):
+        return {
+            "self.user_id": self.user_id,
+            'self.opening_date': self.opening_date,
+            'self.closing_date': self.closing_date,
+            'self.balance': self.balance,
+            "self.interest_rate": self.interest_rate,
+            'self.conditions': self.conditions
+
+        }
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    currency_name = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer(), nullable=False)
+    comment = db.Column(db.Text, nullable=False)
+
+    def to_dict(self):
+        return {
+            "self.currency_name": self.currency_name,
+            'self.rating': self.rating,
+            'self.comment': self.comment
+        }
+
+class TransactionHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Text, nullable=False)
+    operation_type = db.Column(db.Text, nullable=False)
+    currency_num_spent = db.Column(db.Numeric, nullable=False)
+    currency_num_obtained = db.Column(db.Numeric, nullable=False)
+    date_time = db.Column(db.Text, nullable=False)
+    account_from_which_the_transaction = db.Column(db.Numeric, nullable=False)
+    account_on_which_the_transaction = db.Column(db.Numeric, nullable=False)
+    commission = db.Column(db.Numeric, nullable=False)
+
+    def to_dict(self):
+        return {
+            "self.user_id": self.user_id,
+            'self.operation_type': self.operation_type,
+            'self.currency_num_spent': self.currency_num_spent,
+            'self.currency_num_obtained': self.currency_num_obtained,
+            "self.date_time": self.date_time,
+            'self.account_from_which_the_transaction': self.account_from_which_the_transaction,
+            "self. account_on_which_the_transaction": self. account_on_which_the_transaction,
+            'self. commission': self. commission
+        }
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    login = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+
+    def to_dict(self):
+        return {
+            "self.login": self.login,
+            'self.password': self.password
+        }
